45d1e275355133d6cb81753ce719904a218c3a7d
gillesB <baatzgilles@googlemail.com>
2012-04-28 15:58:12 +0200

    fixed rotation

--- azulejo/azulejo.py
+++ azulejo/azulejo.py
@@ -3,20 +3,16 @@ from collections import deque
 import time
 import keybinder
 import configuration
+import operator
 
 maximized_window_geometry = gtk.gdk.get_default_root_window().property_get('_NET_WORKAREA')[2][:4]
 upper_corner = maximized_window_geometry[:2]
 screen_width = maximized_window_geometry[2]
 screen_height = maximized_window_geometry[3]
 
-
-'''because window resizing is not accurate we need a quick dirty workaround'''
-window_geometry_error_margin = 30
-
 geometry_to_use_index = 0
-
-#variable to hold the amount of windows since the last arrangement
-arrangement_size = 0
+windows_deq = deque()
+windows_geo = []
 
 def get_normal_windows_on_current_desktop():
     def m_get_window_from_XID(XID):
@@ -27,16 +23,16 @@ def get_normal_windows_on_current_desktop():
             return True
         return False
     
-    windowsXID = gtk.gdk.get_default_root_window().property_get("_NET_CLIENT_LIST_STACKING")[2]
-    windows = map(m_get_window_from_XID, windowsXID)
+    XIDs = gtk.gdk.get_default_root_window().property_get("_NET_CLIENT_LIST_STACKING")[2]
+    windows = map(m_get_window_from_XID, XIDs)
     filtered_windows = filter(f_normal_window, windows)
     filtered_windows.reverse()
     return filtered_windows
 
 def window_is_on_current_desktop(window):
-    currentDesktop = gtk.gdk.get_default_root_window().property_get("_NET_CURRENT_DESKTOP")[2][0]
-    windowIsOnDesktop = window.property_get('_NET_WM_DESKTOP')[2][0]
-    if currentDesktop == windowIsOnDesktop:
+    current_desktop = gtk.gdk.get_default_root_window().property_get("_NET_CURRENT_DESKTOP")[2][0]
+    window_is_on_desktop = window.property_get('_NET_WM_DESKTOP')[2][0]
+    if current_desktop == window_is_on_desktop:
             return True
     return False
 
@@ -89,10 +85,14 @@ def resize_single_window(keybind, geometries):
         geometry_to_use_index = 0
 
     geometry = map (int, geometries_numeric[geometry_to_use_index])
-    _move_and_resize_window(window, geometry)
+    __move_and_resize_window(window, geometry)
 
 def resize_windows(keybind, arrangement):
-    global arrangement_size
+    global windows_deq, windows_geo
+    
+    windows_deq.clear()
+    windows_geo = []
+    
     arrangement_numeric = parse_arrangement(arrangement)
     filtered_windows = get_normal_windows_on_current_desktop()
     amount_of_windows = len(filtered_windows)     
@@ -103,64 +103,58 @@ def resize_windows(keybind, arrangement):
     i = 0
     arrangement_size = len(arrangement_numeric) #global scope variable, also used to rotate windows
     while i < arrangement_size:
-        geometry_list_args = [0, 255]
-        index = arrangement_size - (i + 1) #we must start in the end in order to keep window order correct
+        index = i #arrangement_size - (i + 1) #we must start in the end in order to keep window order correct
         geometry = map (int, arrangement_numeric[index])
         __move_and_resize_window(filtered_windows[index], geometry)
-        i += 1    
+        windows_deq.append(filtered_windows[index])
+        windows_geo.append(geometry)
+        i += 1
+    
+    #sort geometries of windows in such a way that, a clockwise rotation will be possible    
+    windows_geo.sort(key=operator.itemgetter(0, 1))
+    print windows_geo
+    if(len(windows_geo) == 4):
+        windows_geo_clone = windows_geo[::]
+        windows_geo[1] = windows_geo_clone[2]
+        windows_geo[2] = windows_geo_clone[3]
+        windows_geo[3] = windows_geo_clone[1]
 
 def __move_and_resize_window(window, geometry):
+
+    geometry_clone = geometry[::]
 
     window.unmaximize()
     
     #add the upper corner to the position of the window
     #e.g. position 0,0 is somewhere in the upper panel, so the window has to be positioned just below the panel  
-    geometry[0] += upper_corner[0]
-    geometry[1] += upper_corner[1]
+    geometry_clone[0] += upper_corner[0]
+    geometry_clone[1] += upper_corner[1]
     
     #subtract frame width from window size
     window_frame_widths = window.property_get("_NET_FRAME_EXTENTS")[2]
     window_frame_width_left_and_right = window_frame_widths[0] + window_frame_widths[1]
-    geometry[2] -= window_frame_width_left_and_right
+    geometry_clone[2] -= window_frame_width_left_and_right
     
     window_frame_width_top_and_bottom = window_frame_widths[2] + window_frame_widths[3]
-    geometry[3] -= window_frame_width_top_and_bottom
+    geometry_clone[3] -= window_frame_width_top_and_bottom
     
-    window.move_resize(*geometry)
-
+    window.move_resize(*geometry_clone)
 
 def rotate_windows(keybind, dummy):
-    print "rotation is disabled atm"
-#    global arrangement_size
-#    windows = get_normal_windows_on_current_desktop()
-#    amount_of_windows = len(windows)
-#    
-#    if amount_of_windows > arrangement_size:
-#        windows = windows[:arrangement_size]
-#        
-#    geos = []
-#    for window in windows:
-#        window_geo = window.get_geometry()
-#        window_geo = window_geo[:4]
-#        geos.append(window_geo)
-#        
-#    #do the actual rotations, lets use deque as it's dramatically more efficient than a trivial shift implementation
-#    windows_deq = deque(windows)
-#    windows_deq.rotate(1)
-#      
-#    rotation_len = len(windows_deq)
-#    i = 0
-#    while i < rotation_len:
-#        geometry_list_args = [0, 255]
-#        index = rotation_len - (i + 1) #again, start by the tail
-#        #geometry_list_args.extend(map (int, geos[index]))
-#        geometry = map (int, geos[index])
-#        windows_deq[index].unmaximize()
-#        #windows_deq[index].set_geometry(*geometry_list_args)
-#        __move_and_resize_window(windows_deq[index], geometry)
-#        i += 1
-#    
-#    #(windows_deq[0]).activate(int(time.time())) #not sure why it doesn't work. if uncommented causes other windows beyond the rotated ones to hide behind current ones even after pressing ctrl+tab
+    global windows_deq
+    print windows_deq
+    print windows_geo 
+    
+    rotation_len = len(windows_deq)
+    i = 0
+    while i < rotation_len:
+        window = windows_deq[i]
+        print i, (i + 1 + rotation_len) % rotation_len
+        geometry = windows_geo[(i + 1 + rotation_len) % rotation_len] 
+        __move_and_resize_window(window, geometry)
+        i += 1 
+        
+    windows_deq.rotate(1)
 
 def print_window_info():
     window = gtk.gdk.screen_get_default().get_active_window()
@@ -174,8 +168,7 @@ callable_actions = dict(\
     resize_windows=resize_windows, \
     rotate_windows=rotate_windows, \
     move_single_window=move_single_window   
-)   
-
+)
 
 def dispatcher(dis_param):       
     func = dis_param[0]
